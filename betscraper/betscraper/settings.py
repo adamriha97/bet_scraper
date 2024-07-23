cb9fb86663a6b8798816a4d2b383cda23fcacac9
Adam Říha <adam.riha97@gmail.com>
2024-07-24 00:45:55 +0200

    testing

--- betscraper/betscraper/settings.py
+++ betscraper/betscraper/settings.py
@@ -108,21 +108,21 @@ USER_AGENT="Mozilla/5.0 (X11; Linux x86_64; rv:34.0) Gecko/20100101 Firefox/34.0
 
 # Splash
 # set the Splash local server endpoint
-SPLASH_URL = "http://localhost:8050"
+###SPLASH_URL = "http://localhost:8050"
 
 # enable the Splash downloader middleware and 
 # give it a higher priority than HttpCompressionMiddleware
-DOWNLOADER_MIDDLEWARES = {
-    "scrapy_splash.SplashCookiesMiddleware": 723,
-    "scrapy_splash.SplashMiddleware": 725,
-    "scrapy.downloadermiddlewares.httpcompression.HttpCompressionMiddleware": 810,
-}
+###DOWNLOADER_MIDDLEWARES = {
+###    "scrapy_splash.SplashCookiesMiddleware": 723,
+###    "scrapy_splash.SplashMiddleware": 725,
+###    "scrapy.downloadermiddlewares.httpcompression.HttpCompressionMiddleware": 810,
+###}
 
 # enable the Splash deduplication argument filter to
 # make Scrapy Splash saves spice disk on cached requests
-SPIDER_MIDDLEWARES = {
-    "scrapy_splash.SplashDeduplicateArgsMiddleware": 100,
-}
+###SPIDER_MIDDLEWARES = {
+###    "scrapy_splash.SplashDeduplicateArgsMiddleware": 100,
+###}
 
 # set the Splash deduplication class
-DUPEFILTER_CLASS = "scrapy_splash.SplashAwareDupeFilter"
+###DUPEFILTER_CLASS = "scrapy_splash.SplashAwareDupeFilter"
--- betscraper/betscraper/spiders/spider_betano.py
+++ betscraper/betscraper/spiders/spider_betano.py
@@ -1,25 +1,25 @@
 import scrapy
 import json
+
 from scrapy_splash import SplashRequest
 
+from scrapy_selenium import SeleniumRequest
+from selenium.webdriver.common.by import By
+from selenium.webdriver.support import expected_conditions as EC
 
-lua_script = """
-function main(splash, args)  
-  splash:go(args.url)
-
-  -- custom rendering script logic...
-  
-  return splash:html()
-end
-"""
 
 class SpiderBetanoSpider(scrapy.Spider):
     name = "spider_betano"
-    allowed_domains = ["www.betano.cz", "localhost"]
+    #allowed_domains = ["www.betano.cz", "localhost"]
     start_urls = ["https://www.betano.cz/api/"] # "https://www.betano.cz"
 
     custom_settings = {
-        'FEEDS': {'data_betano.json': {'format': 'json', 'overwrite': True}}
+        'FEEDS': {'data_betano.json': {'format': 'json', 'overwrite': True}},
+        'SELENIUM_DRIVER_NAME': 'chrome',
+        'SELENIUM_DRIVER_ARGUMENTS': ['--headless'], # '--headless'
+        'DOWNLOADER_MIDDLEWARES': {
+            'scrapy_selenium.SeleniumMiddleware': 800
+        }
         }
 
     def parse(self, response):
@@ -29,12 +29,13 @@ class SpiderBetanoSpider(scrapy.Spider):
             #yield {
             #    'origin-url': url
             #}
-            yield SplashRequest(url, callback=self.parse_sport_page, args={"wait": 5}) 
-        #url = 'https://www.betano.cz' + resp_js["structureComponents"]["sports"]["data"][0]["url"] + 'nadchazejici-zapasy-dnes/?sort=StartTime'
+            #yield SplashRequest(url, callback=self.parse_sport_page, args={"wait": 5}) 
+        url = 'https://www.betano.cz' + resp_js["structureComponents"]["sports"]["data"][0]["url"] + 'nadchazejici-zapasy-dnes/?sort=StartTime'
         #yield response.follow(url, callback = self.parse_sport_page)
         #yield scrapy.Request(url, meta={"playwright": True})
         #yield SplashRequest(url, callback=self.parse_sport_page, endpoint="execute", args={"lua_source": lua_script})
         #yield SplashRequest(url, callback=self.parse_sport_page, args={"wait": 15}) 
+        yield SeleniumRequest(url=url, callback=self.parse_sport_page, wait_time=15, wait_until=EC.element_to_be_clickable((By.CLASS_NAME, 'tw-flex'))) # selections__selection tw-flex
 
     def parse_sport_page(self, response):
         yield {
