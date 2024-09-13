06a3981779f915c57076de96fb8ba6a9b21c2b01
Adam Říha <adam.riha97@gmail.com>
2024-09-13 22:03:01 +0200

    spider betano updated

--- betscraper/betscraper/spiders/spider_betano.py
+++ betscraper/betscraper/spiders/spider_betano.py
@@ -10,7 +10,10 @@ class SpiderBetanoSpider(scrapy.Spider):
 
     custom_settings = {
         'FEEDS': {'data_betano.json': {'format': 'json', 'overwrite': True}},
-        'USER_AGENT': "Mozilla/5.0 (X11; Linux x86_64; rv:34.0) Gecko/20100101 Firefox/34.0"
+        'USER_AGENT': "Mozilla/5.0 (X11; Linux x86_64; rv:34.0) Gecko/20100101 Firefox/34.0",
+        'CONCURRENT_REQUESTS': 32, # default 16
+        'CONCURRENT_REQUESTS_PER_DOMAIN': 32, # default 8
+        'DOWNLOAD_DELAY': 0,
         }
     
     def parse(self, response):
@@ -26,19 +29,17 @@ class SpiderBetanoSpider(scrapy.Spider):
         try:
             if response_json["errorCode"] == 301:
                 pass
-                # yield {
-                #     'err': response_json["errorCode"],
-                #     'url': response.url.split('/')[5]
-                # }
         except:
+            sport = response.url.split('/')[5]
             for league in response_json["data"]["blocks"]:
                 for event in league["events"]:
-            #event = response_json["data"]["blocks"][0]['events'][0]
+                    event_url = f'https://www.betano.cz{event["url"]}'
+                    event_startTime = datetime.datetime.fromtimestamp(event["startTime"]/1000)
                     participant_1 = event["participants"][0]["name"]
                     participant_2 = event["participants"][1]["name"]
+                    bet_1 = bet_0 = bet_2 = bet_10 = bet_02 = bet_12 = bet_11 = bet_22 = -1
                     for market in event['markets']:
                         if market["name"].startswith(("Výsledek", "Vítěz")):
-                            bet_1 = bet_0 = bet_2 = -1
                             for selection in market["selections"]:
                                 if selection["name"] == '0':
                                     bet_0 = selection["price"]
@@ -46,14 +47,25 @@ class SpiderBetanoSpider(scrapy.Spider):
                                     bet_1 = selection["price"]
                                 elif selection["name"] in ['2', participant_2]:
                                     bet_2 = selection["price"]
-                    yield {
-                        # 'err': 'OK',
-                        'sport': response.url.split('/')[5],
-                        'event_url': event["url"],
-                        'event_startTime': datetime.datetime.fromtimestamp(event["startTime"]/1000),
-                        'participant_1': participant_1,
-                        'participant_2': participant_2,
-                        'bet_1': bet_1,
-                        'bet_0': bet_0,
-                        'bet_2': bet_2
-                    }
+                    # not a perfect solution because bet_0 can be locked or not available on the site but still relevant option
+                    if (bet_0 == -1) and (not (bet_1 == bet_2 == -1)):
+                        bet_11 = bet_1
+                        bet_1 = -1
+                        bet_22 = bet_2
+                        bet_2 = -1
+                    if not (bet_1 == bet_0 == bet_2 == bet_10 == bet_02 == bet_12 == bet_11 == bet_22 == -1):
+                        yield {
+                            'sport': sport,
+                            'event_url': event_url,
+                            'event_startTime': event_startTime,
+                            'participant_1': participant_1,
+                            'participant_2': participant_2,
+                            'bet_1': bet_1,
+                            'bet_0': bet_0,
+                            'bet_2': bet_2,
+                            'bet_10': bet_10,
+                            'bet_02': bet_02,
+                            'bet_12': bet_12,
+                            'bet_11': bet_11,
+                            'bet_22': bet_22,
+                        }
