9842d19e265015a42ce408e1a689adb7f85d4f8e
Adam Říha <adam.riha97@gmail.com>
2024-10-10 15:18:03 +0200

    TS updated categs gender age

--- old/20241009_item_change_categories/spider_tipsport.py
+++ old/20241009_item_change_categories/spider_tipsport.py
@@ -2,7 +2,6 @@ import scrapy
 import json
 from curl_cffi import requests
 from datetime import datetime
-import re
 
 from betscraper.items import BasicSportEventItem
 
@@ -47,27 +46,13 @@ class SpiderTipsportSpider(scrapy.Spider):
         response_json = json.loads(response_post.text)
         for sport_item in response_json['offerSuperSports']:
             for competition_item in sport_item['tabs'][0]['offerCompetitionAnnuals']:
-                participants_gender = ''
-                try:
-                    sport_gender = competition_item['sportGender']
-                    gender_mapping = {"WOMEN": "zeny", "MEN": "muzi"}
-                    participants_gender = gender_mapping.get(sport_gender, sport_gender)
-                except:
-                    pass
                 for match_item in competition_item['matches']:
                     if match_item['matchType'] == 'MATCH':
                         sport = match_item['nameSuperSport']
-                        primary_category_original = match_item['nameSport']
-                        secondary_category_original = match_item['nameCompetition']
                         event_url = f"https://www.tipsport.cz{match_item['matchUrl']}"
                         event_startTime = datetime.fromisoformat(match_item['datetimeClosed'])
                         participant_1 = match_item['participantHome']
                         participant_2 = match_item['participantVisiting']
-                        participants_age = ''
-                        participant_1_hasAge = re.search(r'U\d{2}', participant_1)
-                        participant_2_hasAge = re.search(r'U\d{2}', participant_2)
-                        if participant_1_hasAge and participant_2_hasAge and (participant_1_hasAge.group(0) == participant_2_hasAge.group(0)):
-                            participants_age = participant_1_hasAge.group(0)
                         bet_1 = bet_0 = bet_2 = bet_10 = bet_02 = bet_12 = bet_11 = bet_22 = -1
                         for opp_row in match_item['oppRows']:
                             for opp_tab in opp_row['oppsTab']:
@@ -91,13 +76,10 @@ class SpiderTipsportSpider(scrapy.Spider):
                         basic_sport_event_item['bookmaker_name'] = 'tipsport'
                         basic_sport_event_item['sport_name'] = ''
                         basic_sport_event_item['sport_name_original'] = sport
-                        basic_sport_event_item['primary_category_original'] = primary_category_original
-                        basic_sport_event_item['secondary_category_original'] = secondary_category_original
+                        basic_sport_event_item['event_url'] = event_url
                         basic_sport_event_item['event_startTime'] = event_startTime
                         basic_sport_event_item['participant_home'] = participant_1
                         basic_sport_event_item['participant_away'] = participant_2
-                        basic_sport_event_item['participants_gender'] = participants_gender
-                        basic_sport_event_item['participants_age'] = participants_age
                         basic_sport_event_item['bet_1'] = bet_1
                         basic_sport_event_item['bet_0'] = bet_0
                         basic_sport_event_item['bet_2'] = bet_2
@@ -106,5 +88,4 @@ class SpiderTipsportSpider(scrapy.Spider):
                         basic_sport_event_item['bet_12'] = bet_12
                         basic_sport_event_item['bet_11'] = bet_11
                         basic_sport_event_item['bet_22'] = bet_22
-                        basic_sport_event_item['event_url'] = event_url
                         yield basic_sport_event_item
