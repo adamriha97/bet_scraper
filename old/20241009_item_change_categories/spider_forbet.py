cbbad0952ffa38c8ff1a0a11bdfc430a9bfba6cd
Adam Říha <adam.riha97@gmail.com>
2024-10-11 00:59:18 +0200

    FB updated categs gender age

--- old/20241009_item_change_categories/spider_forbet.py
+++ old/20241009_item_change_categories/spider_forbet.py
@@ -2,7 +2,6 @@ import scrapy
 import json
 from datetime import datetime
 from zoneinfo import ZoneInfo
-import re
 
 from betscraper.items import BasicSportEventItem
 
@@ -45,25 +44,10 @@ class SpiderForbetSpider(scrapy.Spider):
                 sport_json = response_json['data']['sport'][str(category_json['sportId'])]
                 if str(market['marketId']) == next(iter(response_json['data']['market_list'][str(category_json['sportId'])])):
                     sport = sport_json['name']
-                    primary_category_original = category_json['name']
-                    secondary_category_original = tournament_json['name']
                     event_url = f'https://www.iforbet.cz/prematch/event/{market["eventId"]}'
                     event_startTime = datetime.fromisoformat(event_json['startTs'].replace("Z", "+00:00")).astimezone(ZoneInfo("Europe/Prague"))
                     participant_1 = response_json['data']['competitors'][str(event_json['competitors'][0])]['name']
                     participant_2 = response_json['data']['competitors'][str(event_json['competitors'][1])]['name']
-                    participants_gender = ''
-                    if any('ženy' in string for string in [primary_category_original, secondary_category_original]):
-                        participants_gender = 'zeny'
-                    elif any('muži' in string for string in [primary_category_original, secondary_category_original]):
-                        participants_gender = 'muzi'
-                    participants_age = ''
-                    participant_1_hasAge = re.search(r'U\d{2}', participant_1)
-                    participant_2_hasAge = re.search(r'U\d{2}', participant_2)
-                    if participant_1_hasAge and participant_2_hasAge and (participant_1_hasAge.group(0) == participant_2_hasAge.group(0)):
-                        participants_age = participant_1_hasAge.group(0)
-                    secondary_category_original_hasAge = re.search(r'\d{2} let', secondary_category_original)
-                    if secondary_category_original_hasAge:
-                        participants_age = f"U{secondary_category_original_hasAge.group(0).replace(' let', '')}"
                     # odds_dict = {} # maybe useless
                     odds_list = []
                     for odd in market['odds'].values():
@@ -89,13 +73,10 @@ class SpiderForbetSpider(scrapy.Spider):
                     basic_sport_event_item['bookmaker_name'] = 'forbet'
                     basic_sport_event_item['sport_name'] = ''
                     basic_sport_event_item['sport_name_original'] = sport
-                    basic_sport_event_item['primary_category_original'] = primary_category_original
-                    basic_sport_event_item['secondary_category_original'] = secondary_category_original
+                    basic_sport_event_item['event_url'] = event_url
                     basic_sport_event_item['event_startTime'] = event_startTime
                     basic_sport_event_item['participant_home'] = participant_1
                     basic_sport_event_item['participant_away'] = participant_2
-                    basic_sport_event_item['participants_gender'] = participants_gender
-                    basic_sport_event_item['participants_age'] = participants_age
                     basic_sport_event_item['bet_1'] = bet_1
                     basic_sport_event_item['bet_0'] = bet_0
                     basic_sport_event_item['bet_2'] = bet_2
@@ -104,5 +85,4 @@ class SpiderForbetSpider(scrapy.Spider):
                     basic_sport_event_item['bet_12'] = bet_12
                     basic_sport_event_item['bet_11'] = bet_11
                     basic_sport_event_item['bet_22'] = bet_22
-                    basic_sport_event_item['event_url'] = event_url
                     yield basic_sport_event_item
