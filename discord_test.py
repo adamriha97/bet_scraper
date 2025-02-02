b488a6151df943d220b5a403d5732a62f0b855d6
Adam Říha <adam.riha97@gmail.com>
2025-02-03 00:23:01 +0100

    discord bot updated

--- discord_test.py
+++ discord_test.py
@@ -9,8 +9,12 @@ bot = discord.Bot()
 async def on_ready():
     print(f"{bot.user} is ready and online!")
 
-@bot.slash_command(name="hello", description="Say hello to the bot")
-async def hello(ctx: discord.ApplicationContext):
-    await ctx.respond("Hey!")
+# @bot.slash_command(name="hello", description="Say hello to the bot")
+# async def hello(ctx: discord.ApplicationContext):
+#     await ctx.respond("Hey!")
+
+@bot.command(description="Sends the bot's latency.") # this decorator makes a slash command
+async def ping(ctx): # a slash command will be created with the name "ping"
+    await ctx.respond(f"Pong! Latency is {bot.latency}")
 
 bot.run(os.getenv('MCDUCK_DISCORD_TOKEN'))
